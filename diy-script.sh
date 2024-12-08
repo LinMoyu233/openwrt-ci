35a82e5d424917953e8282f13ce4b878438b539c
羽末 <161136026+LinMoyu233@users.noreply.github.com>
2024-12-08 21:42:06 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -36,8 +36,10 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
-git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+#git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
+git_sparse_clone main https://github.com/Lienol/openwrt-package
+git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci
+#git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
